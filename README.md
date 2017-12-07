4a919391ef3f7c72564219ee0e349ac43f871c72
Shanu Soman <shanusoman1987@gmail.com>
2017-12-07 16:19:17 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# slick
-Fully responsive. Scales with its container. Separate settings per breakpoint Uses CSS3 when available. Fully functional when not. Swipe enabled. Or disabled, if you prefer. Desktop mouse dragging Infinite looping. Fully accessible with arrow key navigation Add, remove, filter &amp; unfilter slides Autoplay, dots, arrows, callbacks, etc...
+http://kenwheeler.github.io/slick/
+
+
+    Fully responsive. Scales with its container.
+    Separate settings per breakpoint
+    Uses CSS3 when available. Fully functional when not.
+    Swipe enabled. Or disabled, if you prefer.
+    Desktop mouse dragging
+    Infinite looping.
+    Fully accessible with arrow key navigation
+    Add, remove, filter & unfilter slides
+    Autoplay, dots, arrows, callbacks, etc...
---
+++ js/main.js
@@ -0,0 +1,52 @@
+$('.fade-sl').slick({
+  dots: true,
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear'
+});
+//
+$('.autoplay').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
+//
+$('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: false,
+    centerMode: false,
+    focusOnSelect: true,
+    responsive: [{
+        breakpoint: 659,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+        }
+    }, {
+        breakpoint: 480,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+        }
+    }]
+});
+
+//appendArrows
+$('.app-arrow').slick({
+    appendArrows: $('.slide-nav'),
+    prevArrow: "<button class='prev'><</button>",
+    nextArrow: "<button class='next'>></button>",
+    fade: true,
+});
